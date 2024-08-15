22e3ec7b02c029914ee8b9419c03367239529ae1
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2024-08-15 16:47:51 -0500

    Reducing limit proccess memory

---
+++ helpers/departamentos.js
@@ -0,0 +1,102 @@
+module.exports = [
+      {
+       departamento: "AMAZONAS",
+       provincias: ["CHACHAPOYAS", "BAGUA", "BONGARA", "CONDORCANQUI", "LUYA", "RODRIGUEZ DE MENDOZA", "UTCUBAMBA"] 
+      },
+      {
+       departamento: "ANCASH",
+       provincias: ["HUARAZ", "AIJA", "ANTONIO RAIMONDI", "ASUNCION", "BOLOGNESI", "CARHUAZ", "CARLOS FERMIN FITZCARRALD", "CASMA", "CORONGO", "HUARI", "HUARMEY", "HUAYLAS", "MARISCAL LUZURIAGA", "OCROS", "PALLASCA", "POMABAMBA", "RECUAY", "SANTA", "SIHUAS", "YUNGAY"] 
+      },
+      {
+       departamento: "APURIMAC",
+       provincias: ["ABANCAY", "ANDAHUAYLAS", "ANTABAMBA", "AYMARAES", "COTABAMBAS", "CHINCHEROS", "GRAU"] 
+      },
+      {
+       departamento: "AREQUIPA",
+       provincias: ["AREQUIPA", "CAMANA", "CARAVELI", "CASTILLA", "CAYLLOMA", "CONDESUYOS", "ISLAY", "LA UNION"] 
+      },
+      {
+       departamento: "AYACUCHO",
+       provincias: ["HUAMANGA", "CANGALLO", "HUANCA SANCOS", "HUANTA", "LA MAR", "LUCANAS", "PARINACOCHAS", "PAUCAR DEL SARA SARA", "SUCRE", "VICTOR FAJARDO", "VILCAS HUAMAN"] 
+      },
+      {
+       departamento: "CAJAMARCA",
+       provincias: ["CAJAMARCA", "CAJABAMBA", "CELENDIN", "CHOTA", "CONTUMAZA", "CUTERVO", "HUALGAYOC", "JAEN", "SAN IGNACIO", "SAN MARCOS", "SAN MIGUEL", "SAN PABLO", "SANTA CRUZ"] 
+      },
+      {
+       departamento: "CALLAO",
+       provincias: ["CALLAO"] 
+      },
+      {
+       departamento: "CUSCO",
+       provincias: ["CUSCO", "ACOMAYO", "ANTA", "CALCA", "CANAS", "CANCHIS", "CHUMBIVILCAS", "ESPINAR", "LA CONVENCION", "PARURO", "PAUCARTAMBO", "QUISPICANCHI", "URUBAMBA"] 
+      },
+      {
+       departamento: "HUANCAVELICA",
+       provincias: ["HUANCAVELICA", "ACOBAMBA", "ANGARAES", "CASTROVIRREYNA", "CHURCAMPA", "HUAYTARA", "TAYACAJA"] 
+      },
+      {
+       departamento: "HUANUCO",
+       provincias: ["HUANUCO", "AMBO", "DOS DE MAYO", "HUACAYBAMBA", "HUAMALIES", "LEONCIO PRADO", "MARAÑON", "PACHITEA", "PUERTO INCA", "LAURICOCHA", "YAROWILCA"] 
+      },
+      {
+       departamento: "ICA",
+       provincias: ["ICA", "CHINCHA", "NAZCA", "PALPA", "PISCO"] 
+      },
+      {
+       departamento: "JUNIN",
+       provincias: ["HUANCAYO", "CONCEPCION", "CHANCHAMAYO", "JAUJA", "JUNIN", "SATIPO", "TARMA", "YAULI", "CHUPACA"] 
+      },
+      {
+       departamento: "LA LIBERTAD",
+       provincias: ["TRUJILLO", "ASCOPE", "BOLIVAR", "CHEPEN", "JULCAN", "OTUZCO", "PACASMAYO", "PATAZ", "SANCHEZ CARRION", "SANTIAGO DE CHUCO", "GRAN CHIMU", "VIRU"] 
+      },
+      {
+       departamento: "LAMBAYEQUE",
+       provincias: ["CHICLAYO", "FERREÑAFE", "LAMBAYEQUE"] 
+      },
+      {
+       departamento: "LIMA",
+       provincias: ["LIMA", "BARRANCA", "CAJATAMBO", "CANTA", "CAÑETE", "HUARAL", "HUAROCHIRI", "HUAURA", "OYON", "YAUYOS"] 
+      },
+      {
+       departamento: "LORETO",
+       provincias: ["MAYNAS", "ALTO AMAZONAS", "LORETO", "MARISCAL RAMON CASTILLA", "REQUENA", "UCAYALI", "DATEM DEL MARAÑON", "PUTUMAYO"] 
+      },
+      {
+       departamento: "MADRE DE DIOS",
+       provincias: ["TAMBOPATA", "MANU", "TAHUAMANU"] 
+      },
+      {
+       departamento: "MOQUEGUA",
+       provincias: ["MARISCAL NIETO", "GENERAL SANCHEZ CERRO", "ILO"] 
+      },
+      {
+       departamento: "PASCO",
+       provincias: ["PASCO", "DANIEL ALCIDES CARRION", "OXAPAMPA"] 
+      },
+      {
+       departamento: "PIURA",
+       provincias: ["PIURA", "AYABACA", "HUANCABAMBA", "MORROPON", "PAITA", "SULLANA", "TALARA", "SECHURA"] 
+      },
+      {
+       departamento: "PUNO",
+       provincias: ["PUNO", "AZANGARO", "CARABAYA", "CHUCUITO", "EL COLLAO", "HUANCANE", "LAMPA", "MELGAR", "MOHO", "SAN ANTONIO DE PUTINA", "SAN ROMAN", "SANDIA", "YUNGUYO"] 
+      },
+      {
+       departamento: "SAN MARTIN",
+       provincias: ["MOYOBAMBA", "BELLAVISTA", "EL DORADO", "HUALLAGA", "LAMAS", "MARISCAL CACERES", "PICOTA", "RIOJA", "SAN MARTIN", "TOCACHE"] 
+      },
+      {
+       departamento: "TACNA",
+       provincias: ["TACNA", "CANDARAVE", "JORGE BASADRE", "TARATA"] 
+      },
+      {
+       departamento: "TUMBES",
+       provincias: ["TUMBES", "CONTRALMIRANTE VILLAR", "ZARUMILLA"] 
+      },
+      {
+       departamento: "UCAYALI",
+       provincias: ["CORONEL PORTILLO", "ATALAYA", "PADRE ABAD", "PURUS"] 
+      }
+    ]
